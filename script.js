c9257720b270960ac057a7f12dcc8d93cc6575ec
Chad Harper <chadharper811@gmail.com>
2024-10-20 14:07:54 -0500

    added javascript file and adding resume info to replace html hard coding

---
+++ script.js
@@ -0,0 +1,64 @@
+const resumeInfo = [
+    {
+        summary: "Entry-level web developer with 20+ years customer service experience in retail, service and maintenance fields. Leverages strong communication and problem solving skills to achieve success in team environments and individual assignments. Learns complex tasks quickly and aims to improve efficiency. Knowledgeable in Java and JavaScript. Seeking to learn and become part of a team to work toward a new career in technology."
+    },
+    {
+        languages: ["Java", "JavaScript", "HTML", "CSS"],
+        frameworks: ["Springboot", "React"],
+        tools: ["MySQL Workbench"]
+    }
+    {
+        education: [
+            {
+                association: "LaunchCode",
+                degree: "Web Development",
+                year: "2024"
+            },
+            {
+                association: "freeCodeCamp",
+                degree: "Responsive Web Design Certification",
+                year: "2024"
+            },
+            {
+                association: "freeCodeCamp",
+                degree: "JavaScript Algorithms and Data Structures Certification",
+                year: "2024"
+            },
+            {
+                association: "Johnson County Community College",
+                degree: "Associate of Science General Sciences",
+                year: "2021"
+            },
+            {
+                association: "Johnson County Community College",
+                degree: "Associate of Applied Science HVAC Residential Service Technician",
+                year: "2013"
+            },
+        ]
+    },
+    {
+        link: "https://chadharper811.github.io/Resume/",
+        icon: "fa-solid fa-file",
+        text: "Resume Site "
+    },
+    {
+        link: "mailto: chadharper811@gmail.com",
+        icon: "fa-solid fa-envelope",
+        text: "Email me "
+    },
+    {
+        link: "tel:913-961-8439",
+        icon: "fa-solid fa-mobile-screen",
+        text: "Call me "
+    },
+    {
+        link: "https://www.linkedin.com/in/chadharper811/",
+        icon: "fa-brands fa-linkedin",
+        text: "LinkedIn "
+    },
+    {
+        link: "https://github.com/ChadHarper811",
+        icon: "fa-brands fa-square-github",
+        text: "GitHub "
+    },
+]
