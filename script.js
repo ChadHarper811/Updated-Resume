646c83822de4a7b786952372d630865c82d4b1c9
Chad Harper <chadharper811@gmail.com>
2025-02-23 11:10:41 -0600

    added maintenace and intern info

--- script.js
+++ script.js
@@ -91,18 +91,18 @@ const resumeInfo = [
                 bullets: ["Utilized strong problem-solving skills to recognize and solve the underlying issues the customer was experiencing with our company’s product.", "Keep warehouse storage areas, build team areas, and individual employee work areas organized to optimize productivity, ease of use, and to ensure team safety.", "Train new hires and employees on best work practices for new and current projects and time management skills.", "Oversee build teams' daily job tasks, productivity focus, final product quality assurance checks and review paperwork for missing information, inaccuracies & overall quality."]
             },
             {
-                title: "Web Development Course",
-                company: "LaunchCode",
-                company_site: "https://www.launchcode.org/",
-                dates: "May 2023 - January 2024",
-                bullets: ["Course Coverage:","Apply foundational coding structures to solve problems using JavaScript and Java.", "Develop full stack web applications that integrate databases to manage user data and a front end in React to display that data.", "Use common developer tools and best practices to write professional-quality code.", "Build software projects from the ground up using in-demand skills and technologies.", "Communicate and demonstrate their professional and technical skills"]
-            },
-            {
                 title: "BookMaster App",
                 company: "LaunchCode - Capstone Project",
                 company_site: "https://www.launchcode.org/",
                 dates: "December 2023 - January 2024",
                 bullets: ["Developed a library management app with a team of developers.", "Implemented check-in and check-out features with a user display." ,"Created book review feature with update and delete functionality in the user dashboard.", "Applied React routing for page routing and styled the front end of the application, including the navigation bar and drop down menus with a quick view of the user's currently checked out books.", "Full Project Tech stack: JavaScript, React, Java, MySQL, Bootstrap, Spring Boot, Google Books API"]
+            },
+            {
+                title: "Web Development Online Course",
+                company: "LaunchCode",
+                company_site: "https://www.launchcode.org/",
+                dates: "May 2023 - January 2024",
+                bullets: ["Course Coverage:","Apply foundational coding structures to solve problems using JavaScript and Java.", "Develop full stack web applications that integrate databases to manage user data and a front end in React to display that data.", "Use common developer tools and best practices to write professional-quality code.", "Build software projects from the ground up using in-demand skills and technologies.", "Communicate and demonstrate their professional and technical skills"]
             },
             {
                 title: "Team Lead: Food/Drink Runners",
@@ -113,14 +113,26 @@ const resumeInfo = [
             {
                 title: "Trainer/Cook/Crew Member",
                 company: "Chipotle",
-                dates: "August 2022 - August 2023",
+                dates: "May 2019 - August 2022",
                 bullets: ["Trained new employees in daily job duties and store functions.", "Updated training with store policies and maintained other employees updated training.","Food preparation, handling and safety.", "Cooking all food for the line and online services areas.", "Maintaining clean and sanitary working conditions while also keeping kitchen, storage, storefront and back of house areas organized.", "Assisting customers in preparing their food and any side items.", "Keeping front line restocked at all times and in range of food safety temperatures.", "Customer service with quick and efficient check out experience."],
+            },
+            {
+                title: "Sustainability Intern",
+                company: "Johnson County Community College",
+                dates: "January 2019 - May 2019",
+                bullets: ["Breaking down and scraping or repurposing all items normally thrown away on campus including new renovation areas (such as desks, computers, furniture, etc).", "Sorting campus recycling (all recycling on campus sorted i.e. “paper”, “plastic”, “metals” etc).", "Everyday food waste pick up from all campus coffee shops and cafeteria.", "Used food waste to produce compost on site for use by agriculture department."],
             },
             {
                 title: "Installation Technician",
                 company: "ITC Service Group LLC for Google Fiber",
-                dates: "February 2016 to December 2018",
+                dates: "February 2016 to January 2019",
                 bullets: ["Keep stock of truck inventory, tools, consumable supplies, and customer documents/pamphlets.", "Maintain and cleaned truck, safety equipment, and uniforms.", "Document detailed work notes, time logs, and paperwork; Including monthly inventory and inspection sheets for company vehicle, tools, and safety equipment.", "Remain up to date with training, pricing, new products, features, in-house information, and future company plans while following company information security protocols", "Install new fiber optic, CAT5e, coaxial, and phone wiring with minimal waste", "Work directly with customers to position and set up equipment for optimal use and customer satisfaction while keeping wiring proficiently concealed.", "Perform job duties while also following local ordinances, building codes, and safety laws."],
+            },
+            {
+                title: "Maintenance Technician",
+                company: "The Preserve Apartments & Willow Creek Apartments",
+                dates: "November 2014 to February 2016",
+                bullets: ["Install/maintain/repair apartments and commercial buildings heating, air conditioning, electrical wiring, electrical conduit, water heaters, plumbing, plumbing appliances, drywall, general appliances, trim work, door frames, cabinets, carpeting, doors and locks, window/door screens, painting (indoor and outdoor), tile, vinyl flooring, concrete, etc.", "Work efficiently in a fast paced atmosphere both independently and as part of a team to complete daily and long term goals", "General grounds work including major and minor landscaping, trash removal, along with ice and snow treatment/removal", "Pool maintenance; including performing chemical testing, recording chemical levels, cleaning, chemical balancing, maintaining water level, and winterizing"],
             },
             
         ]
